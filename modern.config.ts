e8e1ded8866fb915230613f6adcebb5c5b4bc16c
targeral <targeral@outlook.com>
2024-02-19 18:55:40 +0800

    chore: add functions in vercel config

--- modern.config.ts
+++ modern.config.ts
@@ -2,6 +2,11 @@ import { appTools, defineConfig } from '@modern-js/app-tools';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
+  output: {
+    distPath: {
+      server: 'api',
+    },
+  },
   runtime: {
     router: true,
   },
