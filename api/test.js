4fc54e521914fb8d26af33f2511ff4feb4eef99a
targeral <targeral@outlook.com>
2024-02-20 17:09:01 +0800

    chore: update test

--- api/test.js
+++ api/test.js
@@ -1,8 +1,56 @@
 const path = require('path');
-const { Server } = require('@modern-js/prod-server');
+const { createRenderHandler } = require('@modern-js/prod-server');
+const { routes } = require('../dist/route.json');
 
 module.exports = async (req, res) => {
-  const s = new Server({ pwd: path.resolve('..') });
-  const a = await s.init();
-  return a.render(req, res);
+  const route = routes[0];
+  const render = createRenderHandler({
+    distDir: path.join(__dirname, '../dist'),
+    staticGenerate: false,
+    conf: {
+      server: {},
+    },
+  });
+  console.info(render);
+  const renderResult = await render({
+    ctx: {
+      params: {},
+      pathname: '/',
+      host: '',
+      query: {},
+      url: '/',
+      cookieMap: {},
+      headers: {},
+      req: {},
+      res: {
+        set: () => {
+          // ignore
+        },
+      },
+      getReqHeader: () => undefined,
+      resHasHandled: () => false,
+      error: () => {
+        // ignore
+      },
+      serverData: {
+        router: {
+          baseUrl: route.urlPath,
+        },
+      },
+    },
+    route: routes[0],
+    runner: {
+      extendSSRContext: () => {
+        // empty
+      },
+      handleSSRFallback: () => {
+        // empty
+      },
+    },
+  });
+  console.info(renderResult.contentType);
+  console.info(renderResult.content);
+  res.setHeader('Content-Type', renderResult.contentType);
+  res.send(renderResult.content);
+  // res.send('hello');
 };
